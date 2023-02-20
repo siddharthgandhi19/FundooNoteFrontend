1d0bcf3312e9157553ba6842535db86801cc67d6
siddharthgandhi19 <siddharthgandhi19@gmail.com>
2023-02-20 22:09:18 +0530

    [Siddharth]Add:Note Component of Fundoo Note UI Design

--- fundoo-note/src/App.js
+++ fundoo-note/src/App.js
@@ -4,15 +4,17 @@ import SignUp from './pages/signup/SignUp';
 import Header from './components/header/Header'
 import TakeNote1 from './components/TakeNote1/TakeNote1';
 import TakeNote2 from './components/TakeNote2/TakeNote2'
+import TakeNote3 from './components/TakeNote3/TakeNote3'
 
 function App() {
   return (
     <div className="App">
       {/* <SignIn/> */}
       {/* <SignUp/> */}
-      {/* <Header/> */}
-      {/* <TakeNote1/> */}
-      {/* <TakeNote2/> */}
+      <Header/>
+      <TakeNote1/>
+      <TakeNote2/>
+      <TakeNote3/>
     </div>
   );
 }
---
+++ fundoo-note/src/components/TakeNote3/TakeNote3.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
+import PaletteOutlinedIcon from '@mui/icons-material/PaletteOutlined';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import InputBase from '@mui/material/InputBase';
+
+
+export default function TakeNote3() {
+  
+  return (
+    <Card sx={{ maxWidth: 240 }}>
+      <CardHeader
+       avatar={
+        <InputBase id="outlined-basic" placeholder='Title' variant="outlined" multiline fullWidth="true" />
+    }
+        action={
+          <IconButton aria-label="settings">
+            <PushPinOutlinedIcon style={{ color: '#202124' }}/>
+          </IconButton>
+        }
+      /> 
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <NotificationsActiveOutlinedIcon style={{ color: '#202124' }} fontSize="small"/>
+        </IconButton>
+        <IconButton aria-label="share">
+          <PersonAddAltOutlinedIcon style={{ color: '#202124' }} fontSize="small"/>
+        </IconButton>
+        <IconButton aria-label="share">
+          <PaletteOutlinedIcon style={{ color: '#202124' }} fontSize="small"/>
+        </IconButton>
+        <IconButton aria-label="share">
+          <InsertPhotoOutlinedIcon style={{ color: '#202124' }} fontSize="small"/>
+        </IconButton>
+        <IconButton aria-label="share">
+          <ArchiveOutlinedIcon style={{ color: '#202124' }} fontSize="small"/>
+        </IconButton>
+        <IconButton aria-label="share">
+          <MoreVertOutlinedIcon style={{ color: '#202124' }} fontSize="small"/>
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
